8d381349bced021c2ab229d348fdcde7b1522098
unknown <babanialkesh2@gmail.com>
2026-01-02 14:58:51 +0530

    new test cases

--- tests/webtest_val.spec.ts
+++ tests/webtest_val.spec.ts
@@ -16,8 +16,41 @@ await page.locator('//button[@id="submit"]').click();
 
 
 
+});
 
+test('Test invalid login', async ({ page }) => {
+ await page.goto('/practice-test-login/');
+
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Test Login | Practice Test Automation/);
+
+  //clicks on the button 
+await page.locator('//input[@id="username"]').fill('studnt');
+await page.locator('//input[@id="password"]').fill('Passwrd123');
+
+
+await page.locator('//button[@id="submit"]').click();
 
 
 });
+
+
+test('validtion check', async ({ page }) => {
+ await page.goto('/practice-test-login/');
+
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Test Login | Practice Test Automation/);
+
+  //clicks on the button 
+await page.locator('//input[@id="username"]').fill('studnt');
+await page.locator('//input[@id="password"]').fill('Passwrd123');
+
+
+await page.locator('//button[@id="submit"]').click();
+
+
+});
+
 
